975b89fcff42ccab6a0b285090471e8a7d962eb7
nhanksd85 <30594848+nhanksd85@users.noreply.github.com>
2020-09-21 12:12:46 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.showIcon(IconNames.Heart)
+basic.pause(2000)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(500)
+})
